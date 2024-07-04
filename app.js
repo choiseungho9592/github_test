a6af9c342b1394647395602f2d0ef1e65fb0fd3a
choiseungho9592 <choi9592@gmail.com>
2024-07-04 11:01:08 +0900

    [ADD] app.js  Node.js 백엔드 javascript로 해보고싶은 ... https://www.youtube.com/watch?v=LLljwanXlvg&list=PLwsSnOYjEpei9J0zWjwvSWCnHyqxuPLTW&index=3

---
+++ app.js
@@ -0,0 +1,24 @@
+// 3초에 한 번씩 주기적으로
+// setInterval(() => {
+//     console.log('node js 연습중입니다.')
+// }, 3000);
+
+setTimeout(() => {
+    console.log('타임아웃, 한 번만 실행')
+}, 3000);
+
+// //함수
+// function sayHello(name) {
+//         console.log('hello '+name)
+// }
+
+// sayHello('John')
+// sayHello('choiseungho ')
+
+// const v = 10
+
+// if(v > 5) {
+//     console.log('It is a big number')
+// } else {
+//     console.log('작은 숫자야!')
+// }

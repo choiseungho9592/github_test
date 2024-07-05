57cfb635ce6b07a54fccc17b2250090745e4c0ce
choiseungho9592 <choi9592@gmail.com>
2024-07-05 11:18:33 +0900

    [8] Node.js 웹서버 express javascript 폴더pages와 scripts에 각각 html과 js파일 연결방법 테스트

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const express = require('express')   // npm install express --save
 const app = express()
 
+//pages 경로로 들어오는 요청에 대해서는
+//로컬 폴더 __dirname : main.js가 있는 폴더 위치
+//__dirname + '/pages'
+app.use('/scripts', express.static(__dirname+'/scripts'))
+
 app.listen(3000, ()=> {
     console.log('3000포트에서 듣기를 시작했습니다.')
 })
@@ -15,4 +20,9 @@ app.get(`/about`, (req, res)=>{
     console.log(`===>about에 대한 요청들어왔음`)
     //res.send('about에 대한 요청')
     res.sendFile(__dirname+'/pages/about.html')
+})
+app.get(`/working`, (req, res)=>{
+    console.log(`===>working에 대한 요청들어왔음`)
+    //res.send('about에 대한 요청')
+    res.sendFile(__dirname+'/pages/working.html')
 })
